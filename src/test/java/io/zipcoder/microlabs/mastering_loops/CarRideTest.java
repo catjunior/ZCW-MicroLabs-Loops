9ec5f8e9582b69df16872bab4e5aae8fd8a7e563
Matt Wu <mattwu@zipcoder19.local>
2017-05-10 14:13:21 -0400

    CarRideTest outline

--- src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
@@ -1,9 +1,30 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.Scanner;
 
 public class CarRideTest {
 
 
+    @Test
+    public void areWeThereYetTest(){
+        //Given:
+        String input;
+        String expected = "\"Are we there yet?\"\n + No\n\"Are we there yet?\"\nSoon\n\"Are we there yet?\"\nYes\nGood!";
+
+        //When:
+        Scanner userInput = new Scanner(System.in);
+        input = userInput.nextLine().toLowerCase();
+
+
+        CarRide newCustomer = new CarRide ();
+        String actual = newCustomer.areWeThereYet();
+
+        //Then:
+        assertEquals(expected, actual, 0);
+    }
+
 }
+
