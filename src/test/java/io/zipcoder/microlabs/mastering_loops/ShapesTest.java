cfcb3a9019c114b7e8ba536e2f4dc6962099d459
Matt Wu <mattwu@zipcoder19.fios-router.home>
2017-05-09 21:01:42 -0400

    finished triangle

--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -1,28 +1,44 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Assert;
+import org.junit.Before;
 
 public class ShapesTest {
 
-
+    Shapes shape;
+    @Before
+    public void setUp(){ shape = new Shapes(); }
 
     @Test
     public void triangleTest(){
         //: Given
+        String expected = "triangle()\n*** Output ***\n*\n**\n***\n****\n*****";
 
         //: When
+        String actual = shape.triangle();
 
         //: Then
+        assertEquals( expected, actual);
     }
+
+
 
     @Test
     public void tableSquareTest(){
         //: Given
+        String expected = "A 4 x 4 table square\n*** Output ***\n" +
+                            "| 1 |  2 |  3 |  4 |\n" +
+                            "| 2 |  4 |  6 |  8 |\n" +
+                            "| 3 |  6 |  9 | 12 |\n" +
+                            "| 4 |  8 | 12 | 16 |";
 
         //: When
+        String actual = shape.tableSquare();
 
         //: Then
+        assertEquals( expected, actual);
     }
+
 }
