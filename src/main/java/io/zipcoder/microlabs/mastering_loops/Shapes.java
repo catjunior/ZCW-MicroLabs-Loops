3ba92f0864cdf6dc06a233a861102b67f06aea3c
Matt Wu <mattwu@zipcoder19.fios-router.home>
2017-05-09 23:25:28 -0400

    n*n table (n up to 9)

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,6 +3,7 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
+
     public String triangle() {
 
         String result = "triangle()\n*** Output ***\n";
@@ -12,7 +13,6 @@ public class Shapes {
             for (int j = 0; j <= i; j++) {
 
                 result = result + "*";
-
             }
 
             if (i ==4 ){break;} //prevents print an extra line at the end
@@ -23,37 +23,81 @@ public class Shapes {
         return result;
     }
 
+
     public String tableSquare(){
 
         String result = "A 4 x 4 table square\n*** Output ***\n";
 
+        outerloop: // for rows
         for (int i = 1; i < 5; i++){
 
-            for (int j = i; j <= i * 4; j=j+i) {
+            innerloop: // for columns
+            for (int j = i; j < i * 5; j = j+i) {
 
+                if (j >= i * 2 && j < 10) {         // spacing for column 2 and beyond, and the value is < 10
 
-                if (j > 9) {
+                     result = result + "|  " + j + " ";
 
-                    result = result + "| " + j + " "; // spacing for the lower right corner
-                }
+                } else if (j > 9 && j < 16) {
 
-                else {
+                    result = result + "| " + j + " "; // spacing for column 3 and beyond, but value >= 10
+
+                } else if (j == 16) {                 // avoid extra line break at the end
+
+                    result = result + "| " + j + " |";
+                    break outerloop;
+
+                } else {
 
                     result = result + "| " + j + " "; //spacing for the rest of the square
                 }
 
             }
 
-            result = result + "|" + "\n"; // line brake and add | to the row end
+            result = result + "|" + "\n"; // this is where innerloop(row) ends
 
         }
 
         return result;
     }
 
-    /*
+
+
     public String tableSquares(int n){
-        return "";
+
+        String result = "tableSquares(" + n + ")\n*** Output ***\n";
+
+        outerloop: // for rows
+        for (int i = 1; i <= n; i++){
+
+            innerloop: // for columns
+            for (int j = i; j <= i * n; j = j+i) {
+
+                if (j >= i * 2 && j < 10) {         // spacing for column 2 and beyond, and the value is < 10
+
+                    result = result + "|  " + j + " ";
+
+                } else if (j > 9 && j < n * n) {
+
+                    result = result + "| " + j + " "; // spacing for column 3 and beyond, but value >= 10
+
+                } else if (j == n * n) {                 // avoid extra line break at the end
+
+                    result = result + "| " + j + " |";
+                    break outerloop;
+
+                } else {
+
+                    result = result + "| " + j + " "; //spacing for the rest of the square
+                }
+
+            }
+
+            result = result + "|" + "\n"; // this is where innerloop(row) ends
+
+        }
+
+        return result;
     }
-    */
+
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/ShapesTest.java
@@ -11,6 +11,7 @@ public class ShapesTest {
     @Before
     public void setUp(){ shape = new Shapes(); }
 
+
     @Test
     public void triangleTest(){
         //: Given
@@ -22,7 +23,6 @@ public class ShapesTest {
         //: Then
         assertEquals( expected, actual);
     }
-
 
 
     @Test
@@ -39,6 +39,26 @@ public class ShapesTest {
 
         //: Then
         assertEquals( expected, actual);
+    }
+
+
+    @Test
+    public void tableSquaresTest(){
+        //: Given
+        String expected = "tableSquares(6)\n*** Output ***\n" +
+                            "| 1 |  2 |  3 |  4 |  5 |  6 |\n" +
+                            "| 2 |  4 |  6 |  8 | 10 | 12 |\n" +
+                            "| 3 |  6 |  9 | 12 | 15 | 18 |\n" +
+                            "| 4 |  8 | 12 | 16 | 20 | 24 |\n" +
+                            "| 5 | 10 | 15 | 20 | 25 | 30 |\n" +
+                            "| 6 | 12 | 18 | 24 | 30 | 36 |";
+
+        //: When
+        String actual = shape.tableSquares(6);
+
+        //: Then
+        assertEquals( expected, actual);
+
     }
 
 }
