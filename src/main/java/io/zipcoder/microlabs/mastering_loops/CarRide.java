2984a357d71559e756eb42d29e4fe729b8f627a4
Matt Wu <mattwu@zipcoder19.local>
2017-05-10 18:48:14 -0400

    add CarRide

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,36 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
+
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+    Scanner userInput = new Scanner(System.in);
+
+
+    public String areWeThereYet(){
+
+        String input;
+        String finalOutPut;
+
+        do {
+
+            System.out.println("\"Are we there yet?\"");
+            input = userInput.nextLine().toLowerCase();
+
+        } while (!input.equals("yes"));
+
+        System.out.println("Good!");
+        finalOutPut = "Good!";
+
+        return finalOutPut;
+    }
+
+    public static void main(String[] args) {
+
+        CarRide newCustomer = new CarRide();
+        newCustomer.areWeThereYet();
     }
 
 }
+
